5668447e52839654ab2599f74ac48608ded628fd
Ananta Pandu <anpandumail@gmail.com>
2016-04-14 20:08:24 +0700

    vlc#2

---
+++ elementary/vlc.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:mc3man/trusty-media -y
+sudo apt-add-repository ppa:strukturag/libde265 -y
+sudo apt-get update
+sudo apt-get install vlc -y
+sudo apt-get install vlc-plugin-libde265 -y
