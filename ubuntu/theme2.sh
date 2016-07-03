d49f931a13508457500d3ea40d56e9b16f55bf8a
Ananta Pandu <anpandumail@gmail.com>
2016-07-03 13:13:58 +0700

    03 06 2016

---
+++ ubuntu/theme2.sh
@@ -0,0 +1,10 @@
+sudo add-apt-repository ppa:snwh/pulp -y
+sudo apt-get update
+sudo apt-get install paper-gtk-theme -y
+sudo apt-get install paper-icon-theme -y
+
+sudo add-apt-repository ppa:moka/stable -y
+sudo apt-get update
+sudo apt-get install moka-gtk-theme -y
+sudo apt-get install moka-icon-theme -y
+
