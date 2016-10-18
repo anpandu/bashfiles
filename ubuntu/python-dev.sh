957e1af2b05b9c03cd0743dc4704c03cfe385088
pandu <anpandumail@gmail.com>
2016-10-18 17:08:13 +0700

    pydev

--- ubuntu/python-dev.sh
+++ ubuntu/python-dev.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python-pip python-dev build-essential
+sudo apt-get install python-pip python-dev build-essential -y
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
 sudo pip install virtualenv
