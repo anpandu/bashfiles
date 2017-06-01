a6ba3abd839685e4b9367a247cc8c3541cba01c1
ananta pandu <anpandumail@gmail.com>
2017-06-01 15:01:42 +0700

    hadoop install

--- ubuntu/devs/hadoop-install/java.sh
+++ ubuntu/devs/hadoop-install/java.sh
@@ -1,3 +1,5 @@
 apt update -y
-apt install default-jdk -y
+apt install openjdk-8-jdk -y
+apt install openjdk-8-jre -y
+apt install openjdk-8-jdk-headless -y
 java -version
--- ubuntu/devs/hadoop-install/sitexml/replace-site-xml.sh
+++ ubuntu/devs/hadoop-install/sitexml/replace-site-xml.sh
@@ -1,3 +1,4 @@
+chmod 755 -R /usr/local/hadoop_store/hdfs
 mkdir -p /usr/local/hadoop_store/hdfs/namenode
 mkdir -p /usr/local/hadoop_store/hdfs/datanode
 
