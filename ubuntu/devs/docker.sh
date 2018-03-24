92d2183fc4ba2a1bcba004bdb200ec7522561fae
Ananta Pandu Wicaksana <anpandumail@gmail.com>
2018-03-24 11:18:16 +0700

    docker-compose

--- ubuntu/devs/docker.sh
+++ ubuntu/devs/docker.sh
@@ -15,3 +15,8 @@ sudo systemctl status docker
 
 echo ">>> apply ownership"
 sudo usermod -aG docker ${USER}
+
+echo ">>> install docker-compose"
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
