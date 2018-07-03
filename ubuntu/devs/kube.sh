331b2506ff248329cbd45d74f303577172ea849b
Ananta Pandu Wicaksana <anpandumail@gmail.com>
2018-07-03 15:09:28 +0700

    kube

--- ubuntu/devs/kube.sh
+++ ubuntu/devs/kube.sh
@@ -1,10 +1,10 @@
-# virtualbox
+echo "#=== virtualbox"
 wget https://download.virtualbox.org/virtualbox/5.2.14/virtualbox-5.2_5.2.14-123301~Ubuntu~xenial_amd64.deb
 sudo apt install -y virtualbox-5.2_5.2.14-123301~Ubuntu~xenial_amd64.deb
 rm virtualbox-5.2_5.2.14-123301~Ubuntu~xenial_amd64.deb
 
-# kubectl
+echo "#=== kubectl"
 sudo snap install kubectl --classic
 
-# minikube
+echo "#=== minikube"
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
