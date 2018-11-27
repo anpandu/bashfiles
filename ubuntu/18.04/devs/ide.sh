35c7b968e1682f216f3babb5b4e695be0a926a17
Ananta Pandu Wicaksana <anpandumail@gmail.com>
2018-11-27 11:58:08 +0700

    kube

--- ubuntu/18.04/devs/ide.sh
+++ ubuntu/18.04/devs/ide.sh
@@ -3,3 +3,13 @@ sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update -y
 sudo apt-get install sublime-text -y
+
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y code # or code-insiders
+rm microsoft.gpg
