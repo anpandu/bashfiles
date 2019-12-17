f52616cd5ef317b1a913270601f0f731ae65ae46
Ananta Pandu Wicaksana <anpandumail@gmail.com>
2019-12-17 11:52:44 +0700

    kube

--- ubuntu/18.10/devs/kube.sh
+++ ubuntu/18.10/devs/kube.sh
@@ -5,6 +5,6 @@ sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 echo ">>> Install kubectx + kubens"
-git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
