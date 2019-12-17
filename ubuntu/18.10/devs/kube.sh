4b017d5bae905049bd906e084e04fddb0004cf55
Ananta Pandu Wicaksana <anpandumail@gmail.com>
2019-12-17 11:51:58 +0700

    kub

---
+++ ubuntu/18.10/devs/kube.sh
@@ -0,0 +1,10 @@
+
+echo ">>> Install kubectl"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo ">>> Install kubectx + kubens"
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
--- ubuntu/18.10/devs/kubectl.sh
+++
@@ -1,5 +0,0 @@
-
-echo ">>> Install kubectl"
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
